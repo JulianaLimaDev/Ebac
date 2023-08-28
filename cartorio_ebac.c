ead400a94d11c85e3d62893b234c9e0a83b8385a
JulianaLimaDev <143409217+JulianaLimaDev@users.noreply.github.com>
2023-08-28 12:33:12 -0300

    Inclui o comando " sair do sistema"
    Inclui o comando Sair do sistema.

--- cartorio_ebac.c
+++ cartorio_ebac.c
@@ -109,6 +109,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t4 - Sair do sistema!\n\n");
 		printf("Opção: ");//fim do menu
 				
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -129,6 +130,11 @@ int main()
 			deletar();
 			break;
 			
+			case 4:
+			printf ("Obrigada por utilizar o sistema!\n\n");
+			return 0;
+			break;
+				
 			default:
 			printf("Essa opção não esta disponível!\n");
 			system("pause");
