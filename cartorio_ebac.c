a7842619bd476a03a51e6212aff162431b365299
JulianaLimaDev <143409217+JulianaLimaDev@users.noreply.github.com>
2023-08-28 10:17:18 -0300

    Update cartorio_ebac.c
    Não fiz alteração, apenas testei a edição.

--- cartorio_ebac.c
+++ cartorio_ebac.c
@@ -1,7 +1,7 @@
-#include <stdio.h> //biblioteca de comunicaÁ„o com o usuario
-#include <stdlib.h> //biblioteca de alocaÁ„o de espaÁo em memoria
-#include <locale.h> //biblioteca de alocaÁ„o de texto por regi„o
-#include <string.h> //biblioteca respons·vel por cuidar das string
+#include <stdio.h> //biblioteca de comunica√ß√£o com o usuario
+#include <stdlib.h> //biblioteca de aloca√ß√£o de espa√ßo em memoria
+#include <locale.h> //biblioteca de aloca√ß√£o de texto por regi√£o
+#include <string.h> //biblioteca respons√°vel por cuidar das string
 
 int registro()
 {
@@ -73,12 +73,12 @@ int consulta()
 	
 	if(file==NULL)
 	{
-		printf("N„o foi possÌvel abrir o arquivo, n„o localizado!.\n");
+		printf("N√£o foi poss√≠vel abrir o arquivo, n√£o localizado!.\n");
 	}
 	
 	while(fgets(conteudo, 200, file)  != NULL)
 	{
-		printf("\nEssas s„o as informaÁıes do usu·rio:  ");
+		printf("\nEssas s√£o as informa√ß√µes do usu√°rio:  ");
 		printf("%s" , conteudo);
 		printf("\n\n");
 	}
@@ -88,13 +88,13 @@ int consulta()
 
 int deletar()
 {
-	printf("VocÍ escolheu deletar nomes!\n");
+	printf("Voc√™ escolheu deletar nomes!\n");
 	system("pause");	
 }
 
 int main()
 	{
-	int opcao=0; //Definindo vari·veiss
+	int opcao=0; //Definindo vari√°veiss
 	int laco=1;
 	
 	for(laco=1;laco=1;)
@@ -104,19 +104,19 @@ int main()
 
 		setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
 			
-		printf("### CartÛrio da EBAC ###\n\n"); //inicio do menu
-		printf("Escolha a opÁ„o desejada do menu\n\n");
+		printf("### Cart√≥rio da EBAC ###\n\n"); //inicio do menu
+		printf("Escolha a op√ß√£o desejada do menu\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); 
 		printf("\t4 - Sair do sistema\n\n");
-		printf("OpÁ„o: ");//fim do menu
+		printf("Op√ß√£o: ");//fim do menu
 				
-		scanf("%d", &opcao); //armazenando a escolha do usu·rio
+		scanf("%d", &opcao); //armazenando a escolha do usu√°rio
 		
 		system("cls");
 	
-		switch(opcao) //inicio da seleÁ„o
+		switch(opcao) //inicio da sele√ß√£o
 		{
 			case 1:
 			registro();
@@ -136,10 +136,10 @@ int main()
 			break; 
 		
 			default:
-			printf("Essa opÁ„o n„o esta disponÌvel!\n");
+			printf("Essa op√ß√£o n√£o esta dispon√≠vel!\n");
 			system("pause");
 			break;
-		} //fim da seleÁ„o
+		} //fim da sele√ß√£o
 			
 	}	
 }
